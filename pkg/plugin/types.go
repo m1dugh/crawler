da15a0ae6b74d26a2e6bfe392e57e4a7f249cc86
m1dugh <romain103paris@gmail.com>
2022-01-04 18:32:17 +0100

    adds plublic types for crawler plugins

---
+++ pkg/plugin/types.go
@@ -0,0 +1,13 @@
+package plugin
+
+import (
+	crawler "github.com/m1dugh/crawler/internal/crawler"
+	cr_plugin "github.com/m1dugh/crawler/internal/plugin"
+)
+
+type Attachment = crawler.Attachement
+type OnPageResultAdded = cr_plugin.OnPageResultAdded
+
+type CrawlerPluginEntry = cr_plugin.CrawlerPluginEntry
+
+type CrawlerPlugin = cr_plugin.CrawlerPlugin
