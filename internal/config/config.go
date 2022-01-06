c5b7c74d96e726aede1e94e5a63529fa17f2f247
m1dugh <romain103paris@gmail.com>
2022-01-06 22:58:54 +0100

    adds LoadPluginsFromConfig

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,12 @@
 package config
 
 import (
+	"errors"
 	"io/ioutil"
 	"os"
 	"strings"
+
+	crplg "github.com/m1dugh/crawler/internal/plugin"
 
 	yaml "gopkg.in/yaml.v2"
 )
@@ -23,20 +26,28 @@ var ROOT_PATH string = func() string {
 
 var CONFIG_FILE = ROOT_PATH + "/config.yaml"
 
-func GetConfig() *Config {
+func initEmptyFile() {
+	_, err := os.Open(CONFIG_FILE)
+	if err != nil {
+		os.Create(CONFIG_FILE)
+	}
+}
 
+func GetConfig() (*Config, error) {
+
+	initEmptyFile()
 	source, err := ioutil.ReadFile(CONFIG_FILE)
 	if err != nil {
-		return nil
+		return nil, errors.New("config::GetConfig -> could not read file")
 	}
 
 	var config *Config
 	err = yaml.Unmarshal(source, config)
 	if err != nil {
-		return nil
+		return nil, errors.New("config::GetConfig -> could not unmarshal struct")
 	}
 
-	return config
+	return config, nil
 }
 
 func SaveConfig(config Config) bool {
@@ -48,4 +59,20 @@ func SaveConfig(config Config) bool {
 
 	err = ioutil.WriteFile(CONFIG_FILE, bytes, 0777)
 	return err == nil
+}
+
+func LoadPluginsFromConfig() []*crplg.CrawlerPlugin {
+	config, err := GetConfig()
+	if err != nil {
+		return make([]*crplg.CrawlerPlugin, 0)
+	}
+
+	res := make([]*crplg.CrawlerPlugin, 0, len(config.Plugins))
+	for _, pluginConfig := range config.Plugins {
+		if pluginConfig.Active {
+			res = append(res, crplg.GetCrawlerPlugin(pluginConfig.Path))
+		}
+	}
+
+	return res
 }
